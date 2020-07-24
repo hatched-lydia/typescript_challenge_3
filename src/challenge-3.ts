09778d77b23c3002de59228f037d768a1e7ad7eb
hatched-lydia <lydia@hatchedlabs.com>
2020-07-23 17:44:16 -0700

    Complete stringify function definition and pass all specs with javascript

--- src/challenge-3.ts
+++ src/challenge-3.ts
@@ -1,9 +1,38 @@
-type ValidJSON = ValidJSONObject // | ... | ... | ...
+type ValidJSON = ValidJSONObject; // | ... | ... | ...
 
 interface ValidJSONObject {
   // ...
 }
 
-export const stringify = (input: any): string => (
-  '""'
-);
+export const stringify = (input: ValidJSONObject): string => {
+  if (
+    typeof input === "number" ||
+    typeof input === "boolean" ||
+    input === null
+  ) {
+    return `${input}`;
+  }
+
+  if (typeof input === "string") {
+    const inputStrArr = input.split(/\n/);
+
+    if (inputStrArr.length > 1) {
+      return `"${inputStrArr.join(/\n/)}"`.replace(/\//g, "");
+    } else {
+      return `"${input}"`;
+    }
+  }
+
+  let results = [];
+  if (Array.isArray(input)) {
+    input.forEach((element) => results.push(stringify(element)));
+    return `[${results.join(",")}]`;
+  } else {
+    for (let key in input) {
+      if (input[key] !== undefined || typeof input[key] !== "function") {
+        results.push(`${stringify(key)}:${stringify(input[key])}`);
+      }
+    }
+    return `{${results.join(",")}}`;
+  }
+};
